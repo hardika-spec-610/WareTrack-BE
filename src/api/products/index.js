b407b6dd8fbb602b9826edecd6cfcdc46dd50341
hardika-spec-610 <hardika.moradiya1996@gmail.com>
2023-05-02 12:03:57 +0200

    product CRUD endpoints

--- src/api/products/index.js
+++ src/api/products/index.js
@@ -8,4 +8,139 @@ import ProductsModel from "./model.js";
 
 const productRouter = Express.Router();
 
+productRouter.post("/", async (req, res, next) => {
+  try {
+    const product = new ProductsModel(req.body);
+    const { _id } = await product.save();
+    res.status(201).send({ _id });
+  } catch (error) {
+    next(error);
+  }
+});
+
+productRouter.get("/", async (req, res, next) => {
+  try {
+    console.log("req.query", req.query);
+    console.log("q2m", q2m(req.query));
+    const mongoQuery = q2m(req.query);
+    const products = await ProductsModel.find(
+      mongoQuery.criteria,
+      mongoQuery.options.fields
+    )
+      .limit(mongoQuery.options.limit)
+      .skip(mongoQuery.options.skip)
+      .sort(mongoQuery.options.sort);
+    //   .populate({ path: "reviews", select: "comment rate" });
+    const total = await ProductsModel.countDocuments(mongoQuery.criteria);
+    // no matter the order of usage of these methods, Mongo will ALWAYS apply SORT then SKIP then LIMIT
+    res.send({
+      links: mongoQuery.links(process.env.LOCAL_URL + "/products", total),
+      total,
+      numberOfPages: Math.ceil(total / mongoQuery.options.limit),
+      products,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+productRouter.get("/:productId", async (req, res, next) => {
+  try {
+    const products = await ProductsModel.findById(req.params.productId);
+    if (products) {
+      res.send(products);
+    } else {
+      next(
+        createHttpError(
+          404,
+          `Product with id ${req.params.productId} not found!`
+        )
+      );
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+productRouter.put("/:productId", async (req, res, next) => {
+  try {
+    const updatedProduct = await ProductsModel.findByIdAndUpdate(
+      req.params.productId, // WHO
+      req.body, // HOW
+      { new: true, runValidators: true } // OPTIONS. By default findByIdAndUpdate returns the record pre-modification. If you want to get the newly updated one you shall use new: true
+      // By default validation is off in the findByIdAndUpdate --> runValidators: true
+    );
+    if (updatedProduct) {
+      res.send(updatedProduct);
+    } else {
+      next(
+        createHttpError(
+          404,
+          `Product with id ${req.params.productId} not found!`
+        )
+      );
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+productRouter.delete("/:productId", async (req, res, next) => {
+  try {
+    const deletedProduct = await ProductsModel.findByIdAndDelete(
+      req.params.productId
+    );
+    if (deletedProduct) {
+      res.status(204).send();
+    } else {
+      next(
+        createHttpError(
+          404,
+          `Product with id ${req.params.productId} not found!`
+        )
+      );
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+const cloudinaryUploader = multer({
+  storage: new CloudinaryStorage({
+    cloudinary, // cloudinary is going to search for smth in .env vars called process.env.CLOUDINARY_URL
+    params: {
+      folder: "wareTrack/products",
+    },
+  }),
+}).single("imageUrl");
+
+productRouter.post(
+  "/:productId/upload",
+  cloudinaryUploader,
+  async (req, res, next) => {
+    try {
+      const product = await ProductsModel.findByIdAndUpdate(
+        req.params.productId,
+        { imageUrl: req.file.path },
+        { new: true, runValidators: true }
+      );
+      console.log("FILE:", req.file);
+      const category = product.category;
+      console.log("category:", category);
+      if (product) {
+        res.send(product);
+      } else {
+        next(
+          createHttpError(
+            404,
+            `Product with id ${req.params.productId} not found!`
+          )
+        );
+      }
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
 export default productRouter;
